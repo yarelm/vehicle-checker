a886ec4ef7d852e0e96d76071f25c1d3059e7aa0
Yarel Maman <yarel@doit-intl.com>
2021-10-31 15:54:33 +0200

    cosmetics

--- gateway/index.js
+++ gateway/index.js
@@ -13,28 +13,13 @@ const searchId = process.env.SEARCH_ID;
 const searchCx = process.env.SEARCH_CX;
 const workflow = 'mail';
 
-function sleep(ms) {
-    return new Promise(resolve => {
-      setTimeout(resolve, ms);
-    });
-  }
-
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname,'./public/index.html'));
 });
 
 app.post('/', async (req, res) => {
   const name = process.env.NAME || 'World';
-
-  if (!projectId)
-    return console.error('ERROR: GOOGLE_CLOUD_PROJECT is required.');
-
-  if (!searchId)
-  return console.error('ERROR: SEARCH_ID is required.');
-
-  if (!searchCx)
-  return console.error('ERROR: SEARCH_CX is required.');
-
+  
   if (!req.body.car_id)
       return console.error('missing car id');
 
@@ -42,52 +27,39 @@ app.post('/', async (req, res) => {
     return console.error('missing dest email');
 
   // Execute workflow
-    try {
-        const createExecutionRes = await client.createExecution({
-            execution: {
-                argument: JSON.stringify({
-                  car_id: req.body.car_id,
-                  to_email: req.body.to_email,
-                  search_id: searchId,
-                  search_cx: searchCx,
-                }),
-            },
-            parent: client.workflowPath(projectId, 'europe-west4', workflow),
-        });
-        console.log(`Here: ${createExecutionRes}`);
+  try {
+      const createExecutionRes = await client.createExecution({
+          execution: {
+              argument: JSON.stringify({
+                car_id: req.body.car_id,
+                to_email: req.body.to_email,
+                search_id: searchId,
+                search_cx: searchCx,
+              }),
+          },
+          parent: client.workflowPath(projectId, 'europe-west4', workflow),
+      });
+      console.log(`Here: ${createExecutionRes}`);
 
-        const executionName = createExecutionRes[0].name;
-        console.log(`Created execution: ${executionName}`);
-        res.send(`Request dispatched! Request ID: ${executionName}, Please check your mail!`)
-    
-        // // Wait for execution to finish, then print results.
-        // let executionFinished = false;
-        // let backoffDelay = 1000; // Start wait with delay of 1,000 ms
-        // console.log('Poll every second for result...');
-        // while (!executionFinished) {
-        // const [execution] = await client.getExecution({
-        //     name: executionName,
-        // });
-        // executionFinished = execution.state !== 'ACTIVE';
-    
-        // // If we haven't seen the result yet, wait a second.
-        // if (!executionFinished) {
-        //     console.log('- Waiting for results...');
-        //     await sleep(backoffDelay);
-        //     backoffDelay *= 2; // Double the delay to provide exponential backoff.
-        // } else {
-        //     console.log(`Execution finished with state: ${execution.state}`);
-        //     console.log(execution.result);
-        //     res.send(execution.result);
-        //     return;
-        // }
-        // }
-    } catch (e) {
-        console.error(`Error executing workflow: ${e} ${e.stack}`);
-        res.send(`Error in dispatching request!`)
-    }
-
+      const executionName = createExecutionRes[0].name;
+      console.log(`Created execution: ${executionName}`);
+      res.send(`Request dispatched! Request ID: ${executionName}, Please check your mail!`)
+  
+  } catch (e) {
+      console.error(`Error executing workflow: ${e} ${e.stack}`);
+      res.send(`Error in dispatching request!`)
+  }
 });
+
+if (!projectId)
+  return console.error('ERROR: GOOGLE_CLOUD_PROJECT is required.');
+
+if (!searchId)
+  return console.error('ERROR: SEARCH_ID is required.');
+
+if (!searchCx)
+  return console.error('ERROR: SEARCH_CX is required.');
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
